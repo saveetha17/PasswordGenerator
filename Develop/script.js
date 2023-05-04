70eb6fe33d7135758750c7a6925870453d0e7ce1
saveetha17 <saveethakathiravan@gmail.com>
2023-05-04 09:35:05 -0400

    second commit

---
+++ Develop/script.js
@@ -0,0 +1,17 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//var passwordLength = prompt("Please enter the number of characters you want for you new password.  It must be more than 12 but less than 128.");
